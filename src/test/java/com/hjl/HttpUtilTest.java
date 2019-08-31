22ae51d3dc62760c53c90357e4117447858cc888
lege <1814683753@qq.com>
2019-08-31 19:26:03 +0800

    httpClient

--- src/test/java/com/hjl/HttpUtilTest.java
+++ src/test/java/com/hjl/HttpUtilTest.java
@@ -1,10 +1,15 @@
 package com.hjl;
 
 import com.hjl.utils.HttpClientUtil;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author hjl
@@ -18,7 +23,14 @@ public class HttpUtilTest {
     @Test
     public void testHttpUtil() {
         try{
-            String result = HttpClientUtil.post("http://192.168.168.201:6069/api/ps/queryCostCenter");
+            /*String result = HttpClientUtil.post("http://192.168.168.201:6069/api/ps/queryCostCenter");
+            System.out.println("result = " + result);*/
+            List<NameValuePair> list = new ArrayList<>();
+            list.add(new BasicNameValuePair("TaskTitle", "admin"));
+            list.add(new BasicNameValuePair("PageIndex", "0"));
+            list.add(new BasicNameValuePair("PageSize", "13"));
+            list.add(new BasicNameValuePair("orderBy", "0"));
+            String result = HttpClientUtil.getRequest("https://crmoem.jsti.com:8029/api/V1/Basic/GetAllSaleTask",list);
             System.out.println("result = " + result);
         }catch (Exception e){
             e.printStackTrace();

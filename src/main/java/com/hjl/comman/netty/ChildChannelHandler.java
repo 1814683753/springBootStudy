f7cb0df1d5cd4f0c9cb545474b9c31eecd32b9f4
lege <1814683753@qq.com>
2019-09-19 13:15:14 +0800

    netty

---
+++ src/main/java/com/hjl/comman/netty/ChildChannelHandler.java
@@ -0,0 +1,23 @@
+package com.hjl.comman.netty;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Author hjl
+ * @Description 处理类
+ * @Date 2019/9/18 16:57
+ */
+public class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ChildChannelHandler.class);
+
+
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        socketChannel.pipeline().addLast(new ServerHandler());
+    }
+}

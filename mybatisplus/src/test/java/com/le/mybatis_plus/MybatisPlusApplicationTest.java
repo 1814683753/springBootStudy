ff868705d3d7834a4d249f1e0b4b65d8f60bf09a
黄佳乐 <huangjiale@idwzx.com>
2019-11-15 15:34:53 +0800

    mybatisplus 条件构造器使用

--- mybatisplus/src/test/java/com/le/mybatis_plus/MybatisPlusApplicationTest.java
+++ mybatisplus/src/test/java/com/le/mybatis_plus/MybatisPlusApplicationTest.java
@@ -1,16 +1,26 @@
 package com.le.mybatis_plus;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.le.mybatis_plus.entity.User;
 import com.le.mybatis_plus.mapper.UserMapper;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @SpringBootTest
 class MybatisPlusApplicationTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MybatisPlusApplicationTest.class);
     @Autowired
     private UserMapper userMapper;
 
@@ -23,5 +33,168 @@ class MybatisPlusApplicationTest {
         // 遍历输出
         userList.forEach(System.out::println);
     }
+
+    /**
+     * 简单的crud测试
+     */
+    @Test
+    void testCrud(){
+        // 插入
+        User user = new User();
+        user.setName("test");
+        user.setAge(20);
+        user.setEmail("test@11.com");
+        int count = userMapper.insert(user);
+        LOG.info("count : {}",count);
+        user = userMapper.selectById(1);
+        LOG.info("user is : {}",user);
+        //查询
+        List<User> list = userMapper.selectList(null);
+        list.forEach(u -> {
+            // 修改
+            LOG.info("user:{}",u);
+            u.setName(u.getName() + "_u");
+            userMapper.updateById(u);
+        });
+        //删除
+        userMapper.deleteById(6);
+        list = userMapper.selectList(null);
+        list.forEach(u -> LOG.info("user:{}",u));
+    }
+    @Test
+    void queryByQueryWrapper(){
+        // 创建查询wrapper构造器
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        // 设置条件为id=1 and age is not null
+        queryWrapper.eq("id",1).isNotNull("age");
+        User user = userMapper.selectOne(queryWrapper);
+        LOG.info("user : {}",user);
+    }
+    @Test
+    void queryList(){
+        User user = new User();
+        user.setAge(20);
+        // 创建查询wrapper构造器
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper = new QueryWrapper<>();
+        /**
+         * 条件构造器中这些方法中出现的第一个入参boolean condition表示该条件是否加入最后生成的sql中,不传默认是true
+         * 该参数可以用于校验要使用的参数是否付和规范，符合就加入到sql中
+         */
+        queryWrapper.eq(Objects.nonNull(user),"age",user.getAge()).isNotNull("age");
+        // 如果传入null则查询所有
+        List<User> userList = userMapper.selectList(queryWrapper);
+        userList.forEach(u ->{
+            LOG.info("user : {}",u);
+        });
+        userList.clear();
+        // 除了使用构造器外还可以使用map
+        LOG.info("===========map============");
+        // map条件查询,key为列名，value为具体的值
+        Map<String,Object> map = new HashMap<>();
+        map.put("age",20);
+        // 如果传入的参数为null则查询所有
+        userList = userMapper.selectByMap(map);
+        userList.forEach(u ->{
+            LOG.info("user : {}",u);
+        });
+        userList.clear();
+        LOG.info("===========lambdaQuery============");
+        LambdaQueryWrapper<User> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(User::getAge,20).isNotNull(User::getName);
+        // 如果传入null则查询所有
+        userList = userMapper.selectList(lambdaQueryWrapper);
+        userList.forEach(u ->{
+            LOG.info("user : {}",u);
+        });
+    }
+    @Test
+    void queryTest(){
+        // 创建查询wrapper构造器
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("id",1).isNotNull("age");
+        User user = userMapper.selectOne(queryWrapper);
+        LOG.info("user : {}",user);
+        LOG.info("===========map============");
+        // map条件查询,key为列名，value为具体的值
+        Map<String,Object> map = new HashMap<>();
+        map.put("age",20);
+        // 如果传入的参数为null则查询所有
+        List<User> userList = userMapper.selectByMap(map);
+        userList.forEach(u ->{
+            LOG.info("user : {}",u);
+        });
+        userList.clear();
+        LOG.info("=========queryWrapper=====");
+        // 通过条件构造器获取数据集合，相比较与
+        queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("age",20).isNotNull("age");
+        // 如果传入null则查询所有
+        userList = userMapper.selectList(queryWrapper);
+        userList.forEach(u ->{
+            LOG.info("user : {}",u);
+        });
+        userList.clear();
+        LOG.info("===========lambdaQuery===========");
+        LambdaQueryWrapper<User> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(User::getAge,20).isNotNull(User::getName);
+        // 如果传入null则查询所有
+        userList = userMapper.selectList(lambdaQueryWrapper);
+        userList.forEach(u ->{
+            LOG.info("user : {}",u);
+        });
+    }
+    @Test
+    void testInsert(){
+        User user = new User();
+        user.setName("test_8");
+        user.setAge(19);
+        user.setEmail("test@a.com");
+        int count = userMapper.insert(user);
+        LOG.info("插入成功：{}条",count);
+    }
+    @Test
+    void testDelete(){
+        // 使用条件构造器删除符合条件的,使用QueryWrapper和UpdateWrapper都行，没有删除相关的wrapper
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        int count = userMapper.delete(queryWrapper.eq("name","test_8"));
+        LOG.info("删除成功：{}条",count);
+        UpdateWrapper<User> updateWrapper = new UpdateWrapper<>();
+        count = userMapper.delete(updateWrapper.eq("name","Jone_u"));
+        LOG.info("删除成功：{}条",count);
+        // 使用map删除
+        Map<String,Object> map = new HashMap<>();
+        map.put("id",7);
+        count = userMapper.deleteByMap(map);
+        LOG.info("删除成功：{}条",count);
+        // 使用id删除
+        count = userMapper.deleteById(5);
+        LOG.info("删除成功：{}条",count);
+    }
+    @Test
+    void testUpdate(){
+        // 创建相应update构造器
+        UpdateWrapper<User> userUpdateWrapper = new UpdateWrapper<>();
+        // 设置条件将所有年龄为21时将年龄设置成22
+        userUpdateWrapper.eq("age",22).set("age",20);
+        // 根据构造器修改符合条件的数据
+        int count = userMapper.update(new User(),userUpdateWrapper);
+        LOG.info("count : {}",count);
+        List<User> userList = userMapper.selectByMap(null);
+        userList.forEach(u ->{
+            LOG.info("user : {}",u);
+        });
+        LOG.info("=============update by id==========");
+        // 获取要修改的对象
+        User user = userMapper.selectById(1);
+        LOG.info("update before user : {}",user);
+        user.setName("test");
+        // 根据传入对象的id去修改指定的记录
+        count = userMapper.updateById(user);
+        LOG.info("count : {}",count);
+        user = userMapper.selectById(1);
+        LOG.info("updateById after user : {}",user);
+    }
+
 
 }
